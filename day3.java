b3df5bd4eb05231660538bdb61630a93a5b48d7c
AdeHikmah <adehikmahade@gmail.com>
2025-09-10 12:06:37 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,40 @@
+public class day3 {
+    public static void main(String[] args) {
+        //Komentar satu baris 
+        System.out.println("--- Single line Comment ---");
+        System.out.println("Today is Wedneday");
+        System.out.print("Hari ");
+        System.out.print("Rabu");
+        System.out.println(""); // Memberi Jarak Garis Baru
+        //Third day of coding for 100 days
+        
+        /*
+        Multi line comment
+        Biasanya di gunakan untuk penjelasan panjang dalam program
+        */
+        System.out.println("--- Multi line Comment ---");
+        String grup = "Seventeen";
+        String msg = "Boy Grup";
+        System.out.printf("Grup: %-10s | Jenis: %s%n", grup, msg);
+        /*
+        Seventeen adalah boy grup asal korea dan debut pada tahun 2015.
+        Uniknya, walaupun namanya Seventeen, mereka terdiri dari 13 member yaitu:
+        Scoups
+        Jeonghan
+        Joshua
+        Jun 
+        Hoshi
+        Wonwoo
+        Woozi
+        Dk
+        Mingyu
+        The8
+        Seungkwan
+        Vernon 
+        Dino
+        Alasan kenapa namanya Seventeen adalah karena mereka punya konsep.
+        13 member + 3 unit + 1 tim = 17
+        */
+    }
+  
+}

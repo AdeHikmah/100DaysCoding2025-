7075ea5317a24c287052eb65781849ad2bf21285
AdeHikmah <adehikmahade@gmail.com>
2025-09-11 17:23:50 +0800

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,37 @@
+public class day4 {
+    public static void main(String[] args) {
+        // 1. \n (New Line)
+        System.out.println("1. Menggunakan New Line");
+        System.out.println("Baris Pertama\nBaris Kedua\nBaris Ketiga\n");
+
+        // 2. \t (Horizontal Tab)
+        System.out.println("2. Menggunakan Horizontal Tab");
+        System.out.println("Nama\t: Ade Hikmah Asman");
+        System.out.println("Kelas\t: Informatika 2024\n");
+
+        // 3. \\ (Backslash)
+        System.out.println("3. Menggunakan Backslash");
+        System.out.println("Folder ada di: C:\\Program Files\\Java\n");
+
+        // 4. \" (Double Quote)
+        System.out.println("4. Menggunakan Double Quote");
+        System.out.println("\"EXO comeback!\"\n");
+
+        // 5. \' (Single Quote)
+        System.out.println("5. Menggunakan Single Quote");
+        System.out.println("Hari ini adalah Jum\'at\n");
+
+        // 6. \b (Backspace)
+        System.out.println("6. Menggunakan Backspace");
+        System.out.println("Mari belajar bersama\b!\n");
+
+        // 7. \r (Carriage Return)
+        System.out.println("7. Menggunakan Carriage Return");
+        System.out.println("Belajar Java...\rYes!\n");
+
+        // 8. Gabungan
+        System.out.println("8. Menggunakan Gabungan");
+        System.out.println("Dia berkata: \"Ga sabar EXO comeback desember ini!\"");
+        System.out.println("Path: D:\\We\\Are\\One");
+    }
+}

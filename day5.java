1553a43d48b503ee5eab7b35d8337b350bd1270e
AdeHikmah <adehikmahade@gmail.com>
2025-09-12 18:03:38 +0800

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,13 @@
+public class day5 {
+    public static void main(String[] args) {
+        byte umur = 18;
+        short lagu = 200;
+        int bateraiHp = 13;
+        long jumlahPenduduk = 273000000L;
+
+        System.out.printf("%-35s : %d%n", "Umur sekarang", umur);
+        System.out.printf("%-35s : %d%n", "Jumlah lagu di playlist", lagu);
+        System.out.printf("%-35s : %d%%%n", "Baterai hp saya", bateraiHp);
+        System.out.printf("%-35s : %d%n", "Perkiraan jumlah penduduk Indonesia", jumlahPenduduk);
+    }
+}

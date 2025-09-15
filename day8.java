628d7f6d94641a9824dd9593d028c0b3041e4644
AdeHikmah <adehikmahade@gmail.com>
2025-09-15 20:22:06 +0800

    Create day8.java

---
+++ day8.java
@@ -0,0 +1,23 @@
+public class day8 {
+    public static void main(String[] args) {
+        String alasanISC = "Saya masuk ISC karena ingin menambah pengalaman, teman, dan melatih skill ngoding.";
+        String alasanProdi = "Saya memilih prodi Informatika karena tertarik dengan teknologi, komputer, dan pemrograman.";
+        String pilihanJurusan = "Saya tidak merasa salah pilih jurusan, justru semakin yakin dengan pilihan saya.";
+        String kendala = "Untuk PBO saya cukup kesulitan memahami konsep OOP, "
+                       + "sedangkan untuk DDP saya lumayan sulit dalam memahami logika dan membaca soal, "
+                       + "ditambah lagi saya kurang latihan ngoding.";
+
+        // Cetak soal + jawaban  
+        System.out.println("1. Alasan masuk ISC");  
+        System.out.println("   Jawaban: " + alasanISC + "\n");  
+
+        System.out.println("2. Alasan memilih prodi Informatika");  
+        System.out.println("   Jawaban: " + alasanProdi + "\n");  
+
+        System.out.println("3. Apakah merasa salah pilih jurusan atau tidak");  
+        System.out.println("   Jawaban: " + pilihanJurusan + "\n");  
+
+        System.out.println("4. Ceritakan kendala di Prodi Informatika (PBO & DDP)");  
+        System.out.println("   Jawaban: " + kendala);  
+    }
+}

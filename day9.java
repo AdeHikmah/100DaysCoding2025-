d65e7c19f97468f8aa9e09c9e2c120f15895a2b7
AdeHikmah <adehikmahade@gmail.com>
2025-09-16 16:49:37 +0800

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,27 @@
+public class day9 {
+    public static void main(String[] args) {
+        //Deklarasi
+        String nim;
+        String nama;
+        char kelas;
+        
+        //Inisialisasi
+        nim = "D0224027";
+        nama = "Lia";
+        kelas = 'A';
+        System.out.println("--- Nilai Awal ---");
+        System.out.println( nama);
+        System.out.println(nim);
+        System.out.println(kelas);
+        
+        //Update
+        nim = "D0224309";
+        nama = "Hikma";
+        kelas = 'E';
+        
+        System.out.println("--- Update ---");
+        System.out.println( nama);
+        System.out.println(nim);
+        System.out.println(kelas);
+    }
+}

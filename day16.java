03cae4d4913c3a99566988ce4400122783c40509
AdeHikmah <adehikmahade@gmail.com>
2025-09-23 21:00:27 +0800

    Create day16.java

---
+++ day16.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class day16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        //Input jumlah bus
+        System.out.print("Masukkan jumlah bus yang disewa : ");
+        int jumlahBus = sc.nextInt();
+        
+        //Data tetap
+        final int KAPASITAS_BUS = 50; //kursi
+        final int BIAYA_BUS = 7500000; //rupiah
+        
+        //hitung total kapasitas penumpang
+        int totalKapasitas = jumlahBus * KAPASITAS_BUS;
+        
+        //hitung total biaya sewa semua bus
+        int totalBiaya = jumlahBus * BIAYA_BUS;
+        
+        //hitung biaya per kursi/penumpang
+        double biayaPerKursi = totalBiaya / KAPASITAS_BUS;
+        
+        //OUTPUT
+        System.out.println("\n=== HASIL PERHITUNGAN ===");
+        System.out.println("Jumlah Bus yang Disewa : " + jumlahBus);
+        System.out.println("Total Kapasitas Penumpang : " + totalKapasitas + " kursi");
+        System.out.println("Total Biaya Sewa : Rp" + totalBiaya);
+        System.out.println("Biaya per Kursi/Penumpang : Rp" + biayaPerKursi );
+        
+    }
+  
+}

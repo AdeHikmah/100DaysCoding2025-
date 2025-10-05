af5d389e5fe619523d813fed6f2fa4c76b9f010c
AdeHikmah <adehikmahade@gmail.com>
2025-10-05 20:03:45 +0800

    Create day28.java

---
+++ day28.java
@@ -0,0 +1,12 @@
+public class day28 {
+    public static void main(String[] args) {
+        //operator perbandingan 
+        int angka1 = 78;
+        int angka2 = 75;
+        int angka3 = 75;
+        
+        System.out.println(angka1 != angka2);
+        System.out.println(angka2 == angka3);
+        System.out.println(angka3 == angka1);
+    }
+}

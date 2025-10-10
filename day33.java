1a6d7459adb9dad1b7e36df26f120359f9115ad0
AdeHikmah <adehikmahade@gmail.com>
2025-10-10 21:02:59 +0800

    Update day33.java

--- day33.java
+++ day33.java
@@ -6,13 +6,13 @@ public class day33 {
 
         System.out.println("--- SISTEM VERIFIKASI AGEN ---");
 
-        System.out.print("Masukkan level keamanan (1 - 10): ");
+        System.out.print("Masukkan level agen (1 - 10): ");
         int level = sc.nextInt();
 
         System.out.print("Masukkan jumlah misi sukses: ");
         int misi = sc.nextInt();
 
-        System.out.print("Apakah Anda terdeteksi oleh musuh? (true/false): ");
+        System.out.print("Apakah identitas Anda diketahui oleh musuh? (true/false): ");
         boolean terdeteksi = sc.nextBoolean();
 
         // logika
@@ -23,8 +23,8 @@ public class day33 {
 
         // hasil
         System.out.println("\n--- HASIL VERIFIKASI ---");
-        System.out.println("Level cukup? " + levelCukup);
-        System.out.println("Pengalaman cukup? " + pengalaman);
+        System.out.println("Level agen? " + levelCukup);
+        System.out.println("Pengalaman misi yang sudah dilakukan? " + pengalaman);
         System.out.println("Aman dari musuh? " + aman);
         System.out.println("Apakah agen diterima sistem? " + lolos);
     }

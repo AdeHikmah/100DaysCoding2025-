d01e607c5d3f5b4dddc653db11d5ed65528fcf3e
AdeHikmah <adehikmahade@gmail.com>
2025-10-10 20:31:02 +0800

    Create day33.java

---
+++ day33.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class day33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- SISTEM VERIFIKASI AGEN ---");
+
+        System.out.print("Masukkan level keamanan (1 - 10): ");
+        int level = sc.nextInt();
+
+        System.out.print("Masukkan jumlah misi sukses: ");
+        int misi = sc.nextInt();
+
+        System.out.print("Apakah Anda terdeteksi oleh musuh? (true/false): ");
+        boolean terdeteksi = sc.nextBoolean();
+
+        // logika
+        boolean levelCukup = level >= 7;
+        boolean pengalaman = misi >= 5;
+        boolean aman = !terdeteksi;
+        boolean lolos = (levelCukup && pengalaman && aman) || (level == 10);
+
+        // hasil
+        System.out.println("\n--- HASIL VERIFIKASI ---");
+        System.out.println("Level cukup? " + levelCukup);
+        System.out.println("Pengalaman cukup? " + pengalaman);
+        System.out.println("Aman dari musuh? " + aman);
+        System.out.println("Apakah agen diterima sistem? " + lolos);
+    }
+}

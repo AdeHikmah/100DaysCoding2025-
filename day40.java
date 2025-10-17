ce5b78c2ddb8e52422d29e2f2adbc176e75d7a58
AdeHikmah <adehikmahade@gmail.com>
2025-10-17 19:35:28 +0900

    Implement basic calculator in day40.java

---
+++ day40.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class day40 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan angka pertama : ");
+        double angka1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /) : ");
+        char operator = input.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua : ");
+        double angka2 = input.nextDouble();
+        double hasil;
+
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+            System.out.println("Hasil : " + hasil);
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+            System.out.println("Hasil : " + hasil);
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+            System.out.println("Hasil : " + hasil);
+        } else if (operator == '/') {
+            if (angka2 == 0) {
+                System.out.println("Error pembagian dengan angka nol.");
+            } else {
+                hasil = angka1 / angka2;
+                System.out.println("Hasil : " + hasil);
+            }
+        } else {
+            System.out.println("Operator tidak valid");
+        }
+    }
+}

0b176056b9ad8f4c911de2d6a765e4c90a4072e7
AdeHikmah <adehikmahade@gmail.com>
2025-10-19 21:23:42 +0900

    Refactor day41.java to include discount calculation

--- day41.java
+++ day41.java
@@ -1,16 +1,48 @@
 import java.util.Scanner;
-
-public class forI {
-
+public class day41 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int tinggi = input.nextInt();
-
-        for (int i = 1; i <= tinggi; i++) {       // baris
-            for (int j = 1; j <= i; j++) {        // kolom
-                System.out.print("*");
-            }
-            System.out.println(); // pindah baris
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Program Harga Diskon ---");
+        System.out.print("Masukkan jumlah barang : ");
+        int barang = sc.nextInt();
+        System.out.print("Masukkan harga per barang : ");
+        double harga = sc.nextDouble();
+        System.out.print("Apakah Anda member? (true/false) : ");
+        String status = sc.next();
+        sc.nextLine();
+        double total = barang * harga;
+        boolean anggota = Boolean.parseBoolean(status);
+        boolean berhak = (total >= 250000) && (anggota == true);
+        double totalAkhir = 0;
+        double jumlahDiskon;
+        
+        if (berhak) {
+            jumlahDiskon = total * 0.10;
+        }else{
+            jumlahDiskon = 0;
         }
+        totalAkhir = total - jumlahDiskon;
+        System.out.println("\n--- Ringkasan Pembelian ---");
+        System.out.printf("%-15s : Rp %.2f %n", "Total Pembelian ", total);
+        System.out.printf("%-15s : %b %n", "Berhak Diskon 10% ", status);
+        System.out.printf("%-15s : Rp %.2f %n", "Jumlah Diskon", jumlahDiskon );
+        System.out.printf("%-15s : Rp %.2f %n", "Total Bayar", totalAkhir );
+    }
+}
+
+import java.util.Scanner;
+public class Soal2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int S = sc.nextInt();
+        int L = sc.nextInt();
+        int a = (S + L) % 24;
+        int tidur;
+        if (a < 9){
+            tidur = 9 - a;
+        }else{
+            tidur = 0;
+        }
+        System.out.println(tidur);
     }
 }

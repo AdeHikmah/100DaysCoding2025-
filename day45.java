f5abf0e6029f03950e93037c8289b966b700cf59
AdeHikmah <adehikmahade@gmail.com>
2025-10-22 21:40:32 +0900

    Create day45.java

---
+++ day45.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class day45 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka (1-7) : ");
+        int angka = sc.nextInt();
+        String hari;
+        
+        switch (angka){
+            case 1:
+                hari = "Senin";
+                break;
+           case 2:
+                hari = "Selasa";
+                break;
+           case 3:
+                hari = "Rabu";
+                break;
+           case 4:
+                hari = "Kamis";
+                break;
+           case 5:
+                hari = "Jumat";
+                break;
+           case 6:
+                hari = "Sabtu";
+                break;
+           case 7:
+                hari = "Minggu";
+                break;
+           default:
+               hari = "Jumlah hari hanya 7.";
+        }
+        
+        if(angka >=1 && angka <= 7){
+            System.out.println("Sekarang adalah hari " + hari);
+        }else{
+            System.out.println(hari);
+        }
+    }
+}

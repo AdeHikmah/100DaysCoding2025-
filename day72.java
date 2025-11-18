76e24a546ca49067b6832531eadb608db9e16df4
AdeHikmah <adehikmahade@gmail.com>
2025-11-18 18:44:29 +0800

    Update day72.java

--- day72.java
+++ day72.java
@@ -4,7 +4,7 @@ public class day72 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Masukkan jumlah baris: ");
+        System.out.print("Masukkan angka (n) : ");
         int n = sc.nextInt();
 
         for(int i = 1; i <= n; i++){

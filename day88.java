a906b49ecc1ee117f70aae216dcf8fb1deeec85d
AdeHikmah <adehikmahade@gmail.com>
2025-12-04 20:39:33 +0800

    Create day88.java

---
+++ day88.java
@@ -0,0 +1,17 @@
+public class day88 {
+	public static void main(String[] args) {
+		String [] svt = {
+			"Scoups",
+			"Jeonghan",
+			"Joshua",
+			"Jun",
+			"Hoshi",
+			"Wonwoo",
+			"Woozi"
+			};
+			
+			for(String s : svt){
+				System.out.println(s);
+	}
+}
+}

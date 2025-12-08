9e2b400440d59f7ebf74e5769cd6e9330529a3c7
AdeHikmah <adehikmahade@gmail.com>
2025-12-08 20:40:26 +0800

    Create day92.java

---
+++ day92.java
@@ -0,0 +1,9 @@
+public class day92 {
+    public static void main(String[] args) {
+        System.out.println(tambah(9, 2));
+    }
+
+    static int tambah(int x, int y) {
+        return x + y;
+    }
+}	

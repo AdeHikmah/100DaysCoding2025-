b7bfd60f6f4609a8923083b991c1339345f4d1ab
AdeHikmah <adehikmahade@gmail.com>
2025-12-09 19:35:53 +0800

    Add Java program to demonstrate Math functions

---
+++ day93.java
@@ -0,0 +1,11 @@
+public class day93 {
+    public static void main(String[] args) {
+
+        double angka = 3.7;
+
+        System.out.println("Angka asli : " + angka);
+        System.out.println("Hasil ceil  : " + Math.ceil(angka));
+        System.out.println("Hasil floor : " + Math.floor(angka));
+        System.out.println("Hasil round : " + Math.round(angka));
+    }
+}

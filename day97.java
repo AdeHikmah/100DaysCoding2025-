f04bfa16ae26a186559aaf7f059ea0e0687eac2e
AdeHikmah <adehikmahade@gmail.com>
2025-12-13 19:49:26 +0800

    Create day97.java

---
+++ day97.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class day97 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Program Hitung Luas Persegi ---");
+        System.out.print("Masukkan panjang sisi : ");
+        int sisi = sc.nextInt();
+        
+        int luas = hitungLuasPersegi(sisi);
+        
+        System.out.println("\nLuas Persegi dengan sisi " + sisi + " adalah : " + luas);
+    }
+    static int hitungLuasPersegi(int sisi){
+        return sisi * sisi;
+    }
+}
